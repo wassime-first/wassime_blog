3d1fe1a478ccb8e7eabe79b2242f46c4e23488a9
wassime <wassimetestmail@gmail.com>
2024-12-22 19:08:45 +0100

    first blog 1.03

--- main.py
+++ main.py
@@ -281,4 +281,4 @@ def logout():
 if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
